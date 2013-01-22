d0a88c090117e4535eb5ae6af6e9d9d60d4d590a
Tim Lebo <timrdf@gmail.com>
2013-01-22 22:30:45 +0000

    During install: added stub directories and readme files.

--- data/source/csv2rdf4lod-source-me-for-melagrid.sh
+++ data/source/csv2rdf4lod-source-me-for-melagrid.sh
@@ -104,7 +104,7 @@ export CSV2RDF4LOD_CONVERTER=""
 # see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-process-phase:-name
 #
 export CSV2RDF4LOD_BASE_URI="http://logd.tw.rpi.edu"
-export CSV2RDF4LOD_BASE_URI="https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD_BASE_URI#"
+export CSV2RDF4LOD_BASE_URI="http://lod.melagrid.org"
 
 #
 # Customize: A base URI to use in place of CSV2RDF4LOD_BASE_URI
