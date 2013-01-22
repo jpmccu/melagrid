bcb2c111a882b51d4b381b8a648166e10e8b191e
Tim Lebo <timrdf@gmail.com>
2013-01-22 22:51:30 +0000

    During install: added stub directories and readme files.

---
+++ data/source/csv2rdf4lod-source-me-as-melagrid.sh
@@ -0,0 +1,7 @@
+#3> <#> a <http://purl.org/twc/vocab/conversion/CSV2RDF4LOD_environment_variables> ;
+#3>     rdfs:seeAlso 
+#3>     <http://purl.org/twc/page/csv2rdf4lod/distributed_env_vars>,
+#3>     <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Script:-source-me.sh> .
+
+source /home/lebot/prizms/melagrid/data/source/csv2rdf4lod-source-me-for-melagrid.sh
+source /home/lebot/prizms/melagrid/data/source/csv2rdf4lod-source-me-credentials.sh
