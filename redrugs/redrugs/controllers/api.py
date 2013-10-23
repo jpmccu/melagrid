4413e0c9f2e48c091d9e98e47cf70e322f1a81bf
Jim McCusker <jmccusker@5amsolutions.com>
2013-10-23 00:49:45 -0400

    First cut of ReDrugS

---
+++ redrugs/redrugs/controllers/api.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+"""Main Controller"""
+
+from tg import expose, flash, require, url, lurl, request, redirect, tmpl_context, config
+from tg.i18n import ugettext as _, lazy_ugettext as l_
+from redrugs.model import graph
+from redrugs import model
+import rdflib
+from rdflib.query import Result
+
+from redrugs.lib.base import BaseController
+from redrugs.controllers.error import ErrorController
+from tg.controllers import RestController
+from genshi.template import NewTextTemplate
+from genshi.template.base import Context
+
+import collections, math
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+__all__ = ['RootController']
+
+class LRU_Cache:
+
+    def __init__(self, original_function, maxsize=1000):
+        self.original_function = original_function
+        self.maxsize = maxsize
+        self.mapping = {}
+
+        PREV, NEXT, KEY, VALUE = 0, 1, 2, 3         # link fields
+        self.head = [None, None, None, None]        # oldest
+        self.tail = [self.head, None, None, None]   # newest
+        self.head[NEXT] = self.tail
+
+    def __call__(self, *key):
+        PREV, NEXT = 0, 1
+        mapping, head, tail = self.mapping, self.head, self.tail
+
+        link = mapping.get(key, head)
+        if link is head:
+            value = self.original_function(*key)
+            if len(mapping) >= self.maxsize:
+                old_prev, old_next, old_key, old_value = head[NEXT]
+                head[NEXT] = old_next
+                old_next[PREV] = head
+                del mapping[old_key]
+            last = tail[PREV]
+            link = [last, tail, key, value]
+            mapping[key] = last[NEXT] = tail[PREV] = link
+        else:
+            link_prev, link_next, key, value = link
+            link_prev[NEXT] = link_next
+            link_next[PREV] = link_prev
+            last = tail[PREV]
+            last[NEXT] = tail[PREV] = link
+            link[PREV] = last
+            link[NEXT] = tail
+        return value
+
+
+def geomean(nums):
+    return (reduce(lambda x, y: x*y, nums))**(1.0/len(nums))
+
+def logLikelihood(p):
+    return math.atanh(2*p-1)
+
+def confidenceVote(nums):
+    return (math.tanh(sum([math.atanh(2*x-1) for x in nums])) +1)/2
+
+appendQueryTemplate = NewTextTemplate('''
+PREFIX sio: <http://semanticscience.org/resource/>
+PREFIX prov: <http://www.w3.org/ns/prov#>
+prefix go: <http://purl.org/obo/owl/GO#GO_>
+
+SELECT distinct ?participant ?participantLabel ?target ?targetLabel ?interaction ?interactionType ?typeLabel ?probability ?searchEntity ?searchTerm where {
+  let ( ?searchEntity :=
+{% for s in search %}\
+    <${s}>
+{% end %}\
+  )
+  ?searchEntity rdfs:label ?searchTerm.
+
+  ${searchRelation}
+  
+  graph ?assertion {
+    ?interaction sio:has-participant ?participant.
+    ?interaction sio:has-target ?target.
+    ?interaction a ?interactionType.
+  }
+  ?interactionType rdfs:label ?typeLabel.
+  ?assertion sio:SIO_000008 [
+    a sio:SIO_000765;
+    sio:SIO_000300 ?probability;
+  ].
+
+  ?target rdfs:label ?targetLabel.
+  ?participant rdfs:label ?participantLabel.
+} LIMIT 10000''')
+
+searchRelations = [
+    '''?searchEntity sio:has-participant ?target.''',
+    '''[] sio:has-participant ?participant; sio:has-target ?searchEntity.''',
+    '''[] sio:has-participant ?searchEntity; sio:has-target ?participant.''',
+    ]
+
+def mergeByInteraction(edges):
+    def mergeInteractions(interactions):
+        result = interactions[0]
+        result['probability'] = geomean([i['probability'].value for i in interactions])
+        return result
+    
+    byInteraction = collections.defaultdict(list)
+    for edge in edges:
+        byInteraction[(edge['participant'],edge['interaction'],edge['target'])].append(edge)
+    result = map(mergeInteractions, byInteraction.values())
+    return result
+
+def mergeByInteractionType(edges):
+    def mergeInteractions(interactions):
+        result = interactions[0]
+        result['interactions'] = [i['interaction'] for i in interactions]
+        result['likelihood'] = logLikelihood(result['probability'])
+        return result
+    
+    byInteraction = collections.defaultdict(list)
+    for edge in edges:
+        byInteraction[(edge['participant'],edge['interactionType'],edge['target'])].append(edge)
+    result = map(mergeInteractions, byInteraction.values())
+    return result
+
+def addToGraphFn(search):
+    edges = []
+    for searchRelation in searchRelations:
+        q = appendQueryTemplate.generate(Context(search=search, searchRelation=searchRelation)).render()
+        print q
+        resultSet = model.graph.query(q)
+        variables = [x.replace("?","") for x in resultSet.vars]
+        edges.extend([dict([(variables[i],x[i]) for i  in range(len(x))]) for x in resultSet])
+        print len(edges)
+    edges = mergeByInteraction(edges)
+    edges = mergeByInteractionType(edges)
+    return edges
+
+addToGraphCache = LRU_Cache(addToGraphFn,maxsize=100)
+
+def typeaheadFn(search):
+    resultSet = model.graph.query('''prefix bd: <http://www.bigdata.com/rdf/search#>
+    select distinct ?o ?s where {{
+      ?o bd:search "{0}.*" .
+      ?s rdfs:label ?o.
+      FILTER(isURI(?s))
+    }}  limit 10'''.format(search))
+    result = [[y for y in x] for x in resultSet]
+    return result
+
+typeaheadCache = LRU_Cache(typeaheadFn,maxsize=100)
+
+class ApiController(BaseController):
+
+    
+    @expose('json')
+    def addToGraph(self,*args, **kw):
+        search = tuple(kw['uris'].split(","))
+        print search
+        edges = addToGraphCache(search)
+        print len(edges)
+        return dict(edges=edges)
+    
+
+    @expose('json')
+    def typeahead(self, *args,**kw):
+        search = kw['q']
+        print search
+        result = typeaheadFn(search)
+        print result
+        return dict(keywords=result)
---
+++ redrugs/redrugs/public/javascript/redrugs.js
@@ -0,0 +1,212 @@
+var redrugsApp = angular.module('redrugsApp', []);
+
+redrugsApp.controller('ReDrugSCtrl', function ReDrugSCtrl($scope, $http) {
+    $scope.elements = {
+        nodes:[],
+        edges:[]
+    };
+    $scope.nodeMap = {};
+    $scope.edges = [];
+    $scope.resources = {};
+    $scope.searchTerms = "";
+    $scope.searchTermURIs = {};
+    $scope.getNode = function(uri) {
+        var node = $scope.nodeMap[uri];
+        if (!node) {
+            node = $scope.nodeMap[uri] = {
+                group: "nodes",
+                data: {
+                    id: uri
+                }
+            };
+            $scope.elements.nodes.push(node);
+        }
+        return node;
+    }
+    $("#searchBox").typeahead({
+        minLength: 3,
+        items: 10,
+        updater: function(selection) {
+            console.log(selection);
+            $scope.searchTerms = selection;
+            return selection;
+        },
+        source: function(query, process) {
+            console.log(query);
+            $http({method:'GET',url:"/api/typeahead?q="+encodeURIComponent(query)}).success(function(data) {
+                var keywords = data.keywords.map(function(d) {
+                    $scope.searchTermURIs[$.trim(d[0])] = d[1];
+                    return $.trim(d[0]);
+                });
+                process(keywords);
+            });
+        } 
+    });
+    $scope.addToGraph = function(query) {
+        console.log(query);
+        query = query.split(",").map(function(d) {
+            return encodeURIComponent($scope.searchTermURIs[$.trim(d)])
+        }).join(",")
+        console.log(query);
+        $http({method:'GET',url:"/api/addToGraph?uris="+query})
+            .success($scope.appendToGraph);
+    }
+    $scope.graphView = 
+    $scope.appendToGraph = function(result) {
+        console.log(result);
+        $scope.edges = $scope.edges.concat(result.edges);
+        result.edges.forEach(function(row) {
+            if (!row.probability > 0.5) return;
+            var source = $scope.getNode(row.participant);
+            source.data.label = row.participantLabel;
+            var target = $scope.getNode(row.target);
+            target.data.label = row.targetLabel;
+            var edge = {
+                group: "edges",
+                data: $().extend({}, row, {
+                    source:source.data.id,
+                    target:target.data.id
+                })
+            };
+            $scope.elements.edges.push(edge);
+        });
+        var elements = $scope.elements.nodes.concat($scope.elements.edges);
+        console.log(elements);
+        $('#results').cytoscape({
+            layout: "circle",
+            style: cytoscape.stylesheet()
+                .selector('node')
+                .css({
+                    'content': 'data(label)',
+                    'text-valign': 'center',
+                    'color': 'white',
+                    'text-outline-width': 2,
+                    'text-outline-color': '#888'
+                })
+                .selector('edge')
+                .css({
+                    'target-arrow-shape': 'triangle',
+                    'opacity':0.5,
+                    'line-width':"data(likelihood)"
+                })
+                .selector(':selected')
+                .css({
+                    'background-color': 'black',
+                    'line-color': 'black',
+                    'target-arrow-color': 'black',
+                    'source-arrow-color': 'black'
+                })
+                .selector('.faded')
+                .css({
+                    'opacity': 0.25,
+                    'text-opacity': 0
+                }),
+            elements: elements ,
+            ready: function(){
+                $scope.cy = cy = this;
+                // giddy up...
+                console.log(cy.elements());
+                cy.elements().unselectify();
+    
+                cy.on('tap', 'node', function(e){
+                    var node = e.cyTarget; 
+                    var neighborhood = node.neighborhood().add(node);
+                    
+                    cy.elements().addClass('faded');
+                    neighborhood.removeClass('faded');
+                });
+                
+                cy.on('tap', function(e){
+                    if( e.cyTarget === cy ){
+                        cy.elements().removeClass('faded');
+                    }
+                });
+                layoutOptions = {
+                    name: 'breadthfirst',
+                    fit: true, // whether to fit the viewport to the graph
+                    ready: undefined, // callback on layoutready
+                    stop: undefined, // callback on layoutstop
+                    directed: true, // whether the tree is directed downwards (or edges can point in any direction if false)
+                    padding: 30, // padding on fit
+                    circle: false, // put depths in concentric circles if true, put depths top down if false
+                    roots: undefined // the roots of the trees
+                };
+                
+                //cy.layout( layoutOptions );
+                console.log($scope.cy);
+            }
+        });
+        //$scope.updateDisplay();
+    }
+    $scope.result = $("#result");
+    //$scope.svg = d3.select("#graph");
+    //$scope.force = d3.layout.force();
+    //.size([w, h]);
+    //function updateSize() {
+    //     width = $scope.result.width();
+    //     height = $scope.result.height();
+    //     $scope.svg.attr("width",width)
+    //         .attr("height",height);
+    //     //force.stop()
+    //     $scope.force.size([width, height]);
+    //     //    .start();
+    // }
+    // $(window).resize(updateSize);
+    $scope.updateDisplay = function() {
+        // $scope.force.stop()
+        //     .charge(-1000)
+        //     .size([$scope.result.width(), $scope.result.height()])
+        //     .linkStrength(1)
+        //     .linkDistance(30)
+        //     .gravity(0.05)
+        //     .nodes($scope.nodes)
+        //     .links($scope.edges);
+        // $scope.edgeSVG = $scope.makeEdgeSVG($scope.edges, $scope.svg);
+        // $scope.nodeSVG = $scope.makeNodeSVG($scope.nodes, $scope.svg);
+        // $scope.force.start();
+    }
+
+    $scope.makeEdgeSVG = function(edges, svg) {
+        var result = svg.selectAll("line.edge")
+            .data(edges).enter()
+            .append("line")
+            .attr("class","edge")
+            .attr("stroke","black");
+        return svg.selectAll("line.edge");
+    }
+    $scope.makeNodeSVG = function(nodes, svg) {
+        var result = svg.selectAll("g.node")
+            .data(nodes)
+            .enter()
+            .append("g")
+            .attr("class","node")
+        result.append("circle")
+            .attr("r",20)
+            .attr("stroke","black")
+            .attr("fill","steelblue")
+        result.append("text")
+            .style("text-anchor","middle")
+            .style("dominant-baseline","central")
+            .text(function(d) {return d.label});
+        return svg.selectAll("g.node");
+    }
+    // $scope.force.on("tick", function() {
+    //     $scope.edgeSVG
+    //         .attr("x1",function(d) {
+    //             return d.source.x;
+    //         })
+    //         .attr("y1",function(d) {
+    //             return d.source.y;
+    //         })
+    //         .attr("x2",function(d) {
+    //             return d.target.x;
+    //         })
+    //         .attr("y2",function(d) {
+    //             return d.target.y;
+    //         })
+    //     $scope.nodeSVG
+    //         .attr("transform", function(d) {
+    //             return "translate("+d.x+","+d.y+")";
+    //         })
+    // })
+})
