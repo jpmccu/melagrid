9cb33e89142899174991ff23743b89afb564b68d
Jim McCusker <jmccusker@5amsolutions.com>
2014-02-23 00:39:40 -0500

    first cut at phenotype nanopubs.

--- data/source/data-melagrid-org/uniprot-omim/version/retrieve.sh
+++ data/source/data-melagrid-org/uniprot-omim/version/retrieve.sh
@@ -15,7 +15,10 @@
 #3>      <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Automated-creation-of-a-new-Versioned-Dataset>;
 #3> .
 
-$CSV2RDF4LOD_HOME/bin/cr-create-versioned-dataset-dir.sh cr:auto                                               \
-                                                        'http://beta.sparql.uniprot.org/sparql?queryuri=' \
-                                                       --header-line        0                                  \
-                                                       --delimiter         '\t'
+#$CSV2RDF4LOD_HOME/bin/cr-create-versioned-dataset-dir.sh cr:auto                                               \
+#                                                        'http://logd.tw.rpi.edu/sparql.php?query-option=uri&query-uri=https%3A%2F%2Fraw.github.com%2Fjimmccusker%2Fmelagrid%2Fmaster%2Fdata%2Fsource%2Fdata-melagrid-org%2Funiprot-omim%2Fversion%2Fretreive.rq&service-uri=http%3A%2F%2Fbeta.sparql.uniprot.org%2Fsparql&output=csv&callback=&tqx=&tp=' \
+#                                                       --header-line        0                                  \
+#                                                       --delimiter         ','
+version=`date "+%Y-%m-%d"`
+mkdir -p $version/source $version/automatic
+curl 'http://logd.tw.rpi.edu/sparql.php?query-option=uri&query-uri=https%3A%2F%2Fraw.github.com%2Fjimmccusker%2Fmelagrid%2Fmaster%2Fdata%2Fsource%2Fdata-melagrid-org%2Funiprot-omim%2Fversion%2Fretreive.rq&service-uri=http%3A%2F%2Fbeta.sparql.uniprot.org%2Fsparql&output=csv&callback=&tqx=&tp=' -o $version/source/uniprot-omim.csv
